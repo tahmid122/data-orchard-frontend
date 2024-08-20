8b9669d9341496ce3cf0d10396f476f798323fa9
tahmid122 <mdtahmidalam122@gmail.com>
2024-08-20 21:15:03 +0600

    15th  Commit

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Error = () => {
   return (
     <div className="errorPage">
-      <img src="/public/404webp.webp" alt="Error 404" /> <br />
+      <img src="404webp.webp" alt="Error 404" /> <br />
       <h1>Page Not Found</h1>
     </div>
   );
